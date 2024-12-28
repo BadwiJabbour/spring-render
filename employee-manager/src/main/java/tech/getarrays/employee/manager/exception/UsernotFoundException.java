3e374f961f69cb2f6b0256e21210183fc7bae283
BadwiJabbour <99729673+BadwiJabbour@users.noreply.github.com>
2024-12-29 01:16:24 +0200

    Add files via upload

---
+++ employee-manager/src/main/java/tech/getarrays/employee/manager/exception/UsernotFoundException.java
@@ -0,0 +1,7 @@
+package tech.getarrays.employee.manager.exception;
+
+public class UsernotFoundException extends RuntimeException{
+    public UsernotFoundException(String message) {
+    super(message);
+    }
+}
